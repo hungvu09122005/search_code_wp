6e02e32916185c19ef5b13b06cccc9a2aefdc754
Hungvu2005 <vuhung09122005@gmail.com>
2025-09-30 15:48:35 +0700

    update page login home

--- Window/HomePage.xaml.cs
+++ Window/HomePage.xaml.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +7,15 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -27,12 +31,18 @@ public sealed partial class HomePage : Page
     {
         this.InitializeComponent();
     }
+    protected override void OnNavigatedTo(NavigationEventArgs e)
+    {
+        base.OnNavigatedTo(e);
+
+        if (e.Parameter is string name)
+        {
+            WelcomeTextBlock.Text = $"Welcome to Escape room, {name}!";
+        }
+    }
 
     private void StartGame(object sender, RoutedEventArgs e)
     {
-        if (this.Parent is Frame frame)
-        {
-            frame.Navigate(typeof(FirstQuestionPage));
-        }
+        this.Frame.Navigate(typeof(FirstQuestionPage));
     }
 }
--- Window/MainWindow.xaml.cs
+++ Window/MainWindow.xaml.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,8 +7,14 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using WinRT.Interop;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -25,8 +28,18 @@ namespace App1
     {
         public MainWindow()
         {
-            InitializeComponent();
-            RootFrame.Navigate(typeof(HomePage));
+            this.InitializeComponent();
+            var currentWindow = this;
+
+            IntPtr hWnd = WindowNative.GetWindowHandle(this);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+
+            int width = 1000;
+            int height = 800;
+
+            appWindow.Resize(new Windows.Graphics.SizeInt32(width, height));
+            RootFrame.Navigate(typeof(LoginPage));
         }
     }
 }
