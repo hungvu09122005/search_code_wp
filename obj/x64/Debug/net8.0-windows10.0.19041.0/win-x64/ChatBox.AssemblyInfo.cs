fd1926eca93d2ef097601026e87abb268d351526
Dragon <lechihoanglong7@gmail.com>
2025-10-09 20:32:11 +0700

    Test

--- obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/ChatBox.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/ChatBox.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0622d4b9c52dc2298f68cbea181097748be87cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e7cd3cdb3f180624b3e85c8e1a03fb329b52588")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBox")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
